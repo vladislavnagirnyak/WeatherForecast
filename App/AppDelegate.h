06f23bdbb3a05800164e11c90bf396f0937205a0
Влад Нагирняк <vladnagirnak@MacBook-Air-Vlad.local>
2015-10-27 05:02:39 +0200

    Initial Commit

---
+++ App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  App
+//
+//  Created by Влад Нагирняк on 27.10.15.
+//  Copyright (c) 2015 Влад Нагирняк. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
